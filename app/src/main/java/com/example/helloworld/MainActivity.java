a4fcd8cd3927faf527b2ac1b11f431d7c7ea8696
Ilya Makarsky <ilmobdev@gmail.com>
2021-09-07 10:01:22 +0300

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public int x = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        ((TextView)findViewById(R.id.text)).setText(String.valueOf(++x));
+    }
+}
